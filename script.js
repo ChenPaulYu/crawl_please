a621c63f9ac60c017a7a4c0e5e4ac13646203216
ChenPaulYu <beasoftwarengineering@gmail.com>
2021-05-26 15:43:22 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('#yes').click(function() {
+        alert('yes')
+    });
+
+    $('#no').click(function() {
+        alert('no')
+    });
+
+    $('#submit').click(function() {
+        alert('submit')
+    });
+});
